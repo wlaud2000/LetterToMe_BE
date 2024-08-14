6b989e8d5627c89a5250d3c56da041bd1fd9eb4f
Jimyeong Kim <kjm021221@icloud.com>
2024-08-14 21:06:59 +0900

    ✨ feat: 회원 정보 조회 API 구현

--- src/main/java/com/project/lettertome_be/domain/user/controller/UserController.java
+++ src/main/java/com/project/lettertome_be/domain/user/controller/UserController.java
@@ -2,15 +2,16 @@ package com.project.lettertome_be.domain.user.controller;
 
 import com.project.lettertome_be.domain.user.dto.request.SignUpRequestDto;
 import com.project.lettertome_be.domain.user.dto.response.SignUpResponseDto;
+import com.project.lettertome_be.domain.user.dto.response.UserResponseDto;
+import com.project.lettertome_be.domain.user.jwt.userdetails.CustomUserDetails;
+import com.project.lettertome_be.domain.user.service.UserQueryService;
 import com.project.lettertome_be.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,11 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final UserQueryService userQueryService;
 
     @PostMapping("/signup")
     public ResponseEntity<SignUpResponseDto> signUp(@RequestBody @Valid SignUpRequestDto signUpRequestDto) {
 
         SignUpResponseDto responseDto = userService.signUp(signUpRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<UserResponseDto> getUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        String email = userDetails.getUsername();
+        UserResponseDto userResponseDto = userQueryService.getUserByEmail(email);
+        return ResponseEntity.status(HttpStatus.OK).body(userResponseDto);
     }
 }
