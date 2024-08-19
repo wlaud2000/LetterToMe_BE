bf3a6497373b3f6fce0add27f6bbf1928a78fa27
Jimyeong Kim <kjm021221@icloud.com>
2024-08-19 23:36:54 +0900

    ♻️ refactor: 인증용 객체(CustomUserDetails)와 인가용 객체(AuthUser)를 분리

--- src/main/java/com/project/lettertome_be/domain/user/controller/UserController.java
+++ src/main/java/com/project/lettertome_be/domain/user/controller/UserController.java
@@ -6,8 +6,9 @@ import com.project.lettertome_be.domain.user.dto.request.ChangePasswordRequestDt
 import com.project.lettertome_be.domain.user.dto.request.SignUpRequestDto;
 import com.project.lettertome_be.domain.user.dto.response.SignUpResponseDto;
 import com.project.lettertome_be.domain.user.dto.response.UserResponseDto;
-import com.project.lettertome_be.domain.user.entity.User;
-import com.project.lettertome_be.domain.user.jwt.annotation.AuthUser;
+import com.project.lettertome_be.domain.user.jwt.annotation.CurrentUser;
+import com.project.lettertome_be.domain.user.jwt.dto.AuthUser;
+import com.project.lettertome_be.domain.user.service.UserQueryService;
 import com.project.lettertome_be.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,7 @@ import java.util.Map;
 public class UserController {
 
     private final UserService userService;
+    private final UserQueryService userQueryService;
 
     //회원가입
     @PostMapping("/signup")
@@ -34,46 +36,46 @@ public class UserController {
 
     //회원 정보 조회
     @GetMapping("")
-    public ResponseEntity<UserResponseDto> getUser(@AuthUser User user) {
-        UserResponseDto userResponseDto = UserResponseDto.from(user);
+    public ResponseEntity<UserResponseDto> getUser(@CurrentUser AuthUser authUser) {
+        UserResponseDto userResponseDto = userQueryService.getUserByEmail(authUser);
         return ResponseEntity.ok(userResponseDto);
     }
 
     //비밀번호 변경(로컬유저만 해당)
     @PutMapping("/pw")
-    public ResponseEntity<Map<String, String>> changePassword(@AuthUser User user,
+    public ResponseEntity<Map<String, String>> changePassword(@CurrentUser AuthUser authUser,
                                                               @RequestBody @Valid ChangePasswordRequestDto requestDto) {
-        userService.changePassword(user, requestDto);
+        userService.changePassword(authUser, requestDto);
         return ResponseEntity.ok(Map.of("message", "비밀번호가 성공적으로 변경되었습니다. 다시 로그인 해주세요"));
     }
 
     //이메일 변경(로컬유저만 해당)
     @PatchMapping("/email")
-    public ResponseEntity<Map<String, String>> changeEmail(@AuthUser User user,
+    public ResponseEntity<Map<String, String>> changeEmail(@CurrentUser AuthUser authUser,
                                                            @RequestBody @Valid ChangeEmailRequestDto requestDto) {
-        userService.changeEmail(user, requestDto);
+        userService.changeEmail(authUser, requestDto);
         return ResponseEntity.ok(Map.of("message","이메일이 성공적으로 변경되었습니다. 다시 로그인 해주세요."));
     }
 
     //이름 변경
     @PatchMapping("/nickname")
-    public ResponseEntity<Map<String, String>> changeNickName(@AuthUser User user,
+    public ResponseEntity<Map<String, String>> changeNickName(@CurrentUser AuthUser authUser,
                                                               @RequestBody @Valid ChangeNickNameRequestDto requestDto) {
-        userService.changeNickName(user, requestDto);
+        userService.changeNickName(authUser, requestDto);
         return ResponseEntity.ok(Map.of("message", "이름이 성공적으로 변경되었습니다."));
     }
 
     //회원 탈퇴
     @DeleteMapping("")
-    public ResponseEntity<Map<String, String>> deleteUser(@AuthUser User user) {
-        userService.deleteUser(user);
+    public ResponseEntity<Map<String, String>> deleteUser(@CurrentUser AuthUser authUser) {
+        userService.deleteUser(authUser);
         return ResponseEntity.ok(Map.of("message", "회원 탈퇴가 완료되었습니다."));
     }
 
     //테스트
     @GetMapping("/user")
-    public User user(@AuthUser User user) {
-        return user;
+    public AuthUser user(@CurrentUser AuthUser authUser) {
+        return authUser;
     }
 
 }
